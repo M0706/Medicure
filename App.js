08ed03882c5fcd68a1e9003c7ea7bd58215b3838
killbotXD <sonawane.1@iitj.ac.in>
2020-03-04 23:42:02 +0530

    add stack navigator

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from './screens/login'
+import SignUp from "./screens/signup";
 import Aux from './hoc/Aux'
+import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
 
 const Drawer = createDrawerNavigator();
 
@@ -23,14 +27,18 @@ function DrawerContent() {
 //     </View>
 //   );
 // }
-
+const Stack = createStackNavigator();
 export default function App(){
   return (
     <Aux>
     <NavigationContainer>
-          <Drawer.Navigator drawerContent={() => <DrawerContent />}>
-      <Drawer.Screen name="Home" component={Login} />
-    </Drawer.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="signUp" component={SignUp}/>
+      </Stack.Navigator>
+
     </NavigationContainer>
     </Aux>
   );
