9c89a149970c56b58748809835453848796d924f
Manav Garg <garg.6@iitj.ac.in>
2020-03-04 22:05:12 +0530

    temporary commit

--- screens/loginpage/loginpage.js
+++ screens/loginpage/loginpage.js
@@ -38,49 +38,86 @@ class LoginPage extends React.Component {
         }
     }
     render(){
+      const { loggingIn } = this.props;
+      const { username, password, submitted } = this.state;
         return (
           <View style={styles.container}>
-            <TextInput
-              placeholder="Phone Number"
-              style={styles.input}
-              value={this.state.phone}
-              onChangeText={this.handlechange('phone')}
-              keyboardType='number-pad'
-              
-              />
-              <TextInput
-              placeholder="Name"
-              style={styles.input}
-              value={this.state.name}
-              onChangeText={this.handlechange('name')}
-              />
-              <TouchableOpacity onPress={this.submit}>
-                <Text style={styles.BtnType}>Enter</Text>
-              </TouchableOpacity>
-           
-          </View>
-        );
-      }
-      }
+    <Card style={{elevation: 8}}>
+    <Card.Title title="Login" titleStyle={{ textDecorationColor: 'yellow',paddingLeft: 20,paddingTop: 10}}/>
+
+    <Card.Content>
+    <TextInput styles={styles.TextInput}
+      label='Phone'
+      clear
+      value={this.state.phone}
+      onChangeText={this.handlechange('phone')}
+      keyboardType='number-pad'
+      mode="outlined"
+      icon="camera"
+      theme={{
+         colors: {
+                     background: '#fff'
+            }
+      }}
+
+    />
+    <TextInput  styles={styles.TextInput}
+    label='Password'
+    focused
+    clear
+    value={this.state.name}
+        onChangeText={this.handlechange('name')}
+    mode="outlined"
+    theme={{
+         colors: {
+                     background: '#fff'
+            }
+      }}
+
+    />
+    </Card.Content>
+
+    <Card.Actions style={{justifyContent: 'center'}}>
+    <TouchableOpacity onPress={this.submit}>
+    <IconButton
+    icon="arrow-right"
+    size={30}
+    color="white"
+    style={{backgroundColor: "red",borderRadius:30,height: 60,width:60 ,marginBottom:  -35,alignItems: 'center',justifyContent: 'center'}}/>
+</TouchableOpacity>
+
+
+
+    </Card.Actions>
+  </Card>
+
+
+    </View>
+  );
+}
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    input:{
-      padding:10,
-      borderWidth:1,
-      width:'90%',
-      marginBottom:10
-  
-    },
-    BtnType:{
-      color:'darkblue',
-      fontSize:20
-    
-    }
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 30,
+    justifyContent: 'center',
+    marginTop: -100
+  },
+  input:{
+    padding:10,
+    borderWidth:1,
+    width:'90%',
+    marginBottom:10
+
+  },
+  BtnType:{
+    color:'darkblue',
+    fontSize:20
+
+  },
+  TextInput:{
+    padding:10
+  }
+});
