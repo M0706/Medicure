ce3fa64b63347d0c68bea59c0353c011dce21b4e
killbotXD <sonawane.1@iitj.ac.in>
2020-03-06 06:58:47 +0530

    fix merge conflicts in loginpage

--- screens/loginpage/loginpage.js
+++ screens/loginpage/loginpage.js
@@ -1,14 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-//import { connect } from 'redux';
-import { StyleSheet,
-    Alert, 
-=======
 //import { connect } from 'react-redux';
 import {
     StyleSheet,
     Alert,
->>>>>>> df71761ca9bb2fbf15aa853ebd8a281ef6afb8c4
     Text,
     TouchableOpacity,
     TextInput, View, AsyncStorage
@@ -39,99 +33,7 @@ export default class LoginPage extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
 
-<<<<<<< HEAD
-        this.setState({ submitted: true });
-        const { emailid, password } = this.state;
-        if (emailid && password) {
-            this.props.login(emailid, password);
-        }
-    }
 
-    CheckTextInput = () => {
-      if(this.state.emailid !=''){
-        if(this.state.password != ''){
-          alert('Success')
-        }
-        else{
-          alert('Please enter password')
-        }
-        }else{
-          alert('Please enter emailid')
-        }
-      }
-  
-
-    render(){
-      const { loggingIn } = this.props;
-      const { emailid, password, submitted } = this.state;
-        return (
-          <View style={styles.container}>
-          <Card style={{elevation: 8}}>
-          <Card.Title title="Login" titleStyle={{ textDecorationColor: 'yellow',paddingLeft: 20,paddingTop: 10}}/>
-
-    <Card.Content>
-    <TextInput styles={styles.TextInput}
-      label='Phone'
-      clear
-      value={this.state.phone}
-      onChangeText={this.handlechange('phone')}
-      keyboardType='number-pad'
-      mode="outlined"
-      icon="camera"
-      theme={{
-         colors: {
-                     background: '#fff'
-            }
-            }}
-      />
-    <TextInput  styles={styles.TextInput}
-      label='Password'
-      focused
-      clear
-      value={this.state.name}
-         onChangeText={this.handlechange('name')}
-      mode="outlined"
-      theme={{
-         colors: {
-                     background: '#fff'
-            }
-      }}
-
-    />
-    </Card.Content>
-
-    <Card.Actions style={{justifyContent: 'center'}}>
-    <TouchableOpacity onPress={this.submit}>
-    <IconButton
-    icon="arrow-right"
-    size={30}
-    color="white"
-    onPress={this.CheckTextInput}
-    style={{backgroundColor: "red",borderRadius:30,height: 60,width:60 ,marginBottom:  -35,alignItems: 'center',justifyContent: 'center'}}/>
-</TouchableOpacity>
-</Card.Actions>
-  </Card>
-</View>
-  );
-}
-}
-
-function mapState(state) {
-  const { loggingIn } = state.authentication;
-  return { loggingIn };
-}
-
-const actionCreators = {
-  login: userActions.login,
-  logout: userActions.logout
-};
-
-const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
-export default LoginPage ;
-
-
-
-=======
         this.setState({submitted: true});
         const {username, password} = this.state;
         if (username && password) {
@@ -206,7 +108,7 @@ export default LoginPage ;
         );
     }
 }
->>>>>>> df71761ca9bb2fbf15aa853ebd8a281ef6afb8c4
+
 
 const styles = StyleSheet.create({
     container: {
