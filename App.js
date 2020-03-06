ea42cd3b9f5af2f7cd73f9af59fd75912dc8ce8d
killbotXD <sonawane.1@iitj.ac.in>
2020-03-06 06:56:22 +0530

    fix merge conflicts

--- App.js
+++ App.js
@@ -4,12 +4,8 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-<<<<<<< HEAD
-import LoginPage from './screens/Loginpage/loginpage'
-=======
-import Login from './screens/login'
+import Login from './screens/login';
 import SignUp from "./screens/signup";
->>>>>>> df71761ca9bb2fbf15aa853ebd8a281ef6afb8c4
 import Aux from './hoc/Aux'
 import StackNavigator from "@react-navigation/stack/src/navigators/createStackNavigator";
 
@@ -36,11 +32,6 @@ export default function App(){
   return (
     <Aux>
     <NavigationContainer>
-<<<<<<< HEAD
-          <Drawer.Navigator drawerContent={() => <DrawerContent />}>
-      <Drawer.Screen name="Home" component={LoginPage} />
-    </Drawer.Navigator>
-=======
       <Stack.Navigator screenOptions={{
         headerShown: false
       }}>
@@ -48,7 +39,6 @@ export default function App(){
         <Stack.Screen name="signUp" component={SignUp}/>
       </Stack.Navigator>
 
->>>>>>> df71761ca9bb2fbf15aa853ebd8a281ef6afb8c4
     </NavigationContainer>
     </Aux>
   );
