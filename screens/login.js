884d4771be7ccacd5e63798a1cdf9a1be9fbb1a5
killbotXD <sonawane.1@iitj.ac.in>
2020-03-06 06:54:53 +0530

    created final  login screen

--- screens/login.js
+++ screens/login.js
@@ -1,133 +1,210 @@
 import React, {Component} from 'react';
 import {
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View, AsyncStorage
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  View, AsyncStorage
 } from 'react-native';
-import {Avatar, IconButton, Card, Title, Paragraph, TextInput, Colors} from 'react-native-paper';
+import {IconButton, Card, TextInput} from 'react-native-paper';
 import 'react-native-gesture-handler';
 
 export default class loginScreen extends Component {
 
-    state = {
-        phone: '',
-        name: ''
-    }
+  state = {
+    phone: '',
+    name: ''
+  }
 
-    handlechange = key => val => {
-        this.setState({
-            [key]: val
-        })
-    }
-    submit = async () => {
+  handlechange = key => val => {
+    this.setState({
+      [key]: val
+    })
+  }
+  submit = async () => {
 
-        //save user data
-        //alert(this.state.phone + '\n' + this.state.name)
-        await AsyncStorage.setItem("userphone", JSON.stringify(this.state.phone))
+    //save user data
+    //alert(this.state.phone + '\n' + this.state.name)
+    await AsyncStorage.setItem("userphone", JSON.stringify(this.state.phone))
 
-    }
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Card style={{elevation: 8}}>
-                    <Card.Title title="Login"
-                                titleStyle={{textDecorationColor: 'yellow', paddingLeft: 20, paddingTop: 10}}/>
-
-                    <Card.Content>
-                        <TextInput
-                            label='Email'
-                            clear
-                            value={this.state.email}
-                            onChangeText={this.handlechange('phone')}
-                            mode="outlined"
-                            icon="camera"
-                            style={{padding: 10}}
-                            theme={{
-                                colors: {
-                                    background: '#fff'
-                                }
-                            }}
-
-                        />
-                        <TextInput
-                            label='Password'
-                            focused
-                            clear
-                            value={this.state.name}
-                            onChangeText={this.handlechange('name')}
-                            mode="outlined"
-                            style={{padding: 10}}
-                            theme={{
-                                colors: {
-                                    background: '#fff'
-                                }
-                            }}
-
-                        />
-                        <TouchableOpacity>
-                            <View style={{justifyContent: 'flex-end', color: 'red'}}>
-                                <Text onPress={() => this.props.navigation.navigate("signUp")}
-                                      style={{
-                                          justifyContent: 'flex-end',
-                                          color: '#8334eb',
-                                          paddingLeft: 20,
-                                          paddingBottom: 4
-                                      }}>
-                                    New User? Sign Up.
-                                </Text>
-                            </View>
-
-                        </TouchableOpacity>
-                    </Card.Content>
-
-                    <Card.Actions style={{justifyContent: 'center'}}>
-                        <TouchableOpacity onPress={this.submit}>
-                            <IconButton
-                                icon="arrow-right"
-                                size={30}
-                                color="white"
-                                style={styles.IconButton}/>
-                        </TouchableOpacity>
-
-
-                    </Card.Actions>
-                </Card>
-
-
+  render() {
+    return (
+        <View style={{flex: 1}}>
+          <View style={styles.container2}>
+            <Image
+                style={{
+                  width: '100%',
+                  height: '100%'
+                }}
+                source={{uri: 'https://resize.hswstatic.com/w_907/gif/becoming-doctor.jpg'}}
+            />
+            <View style={styles.overlayStyle}>
+              <Text style={styles.overlayTextStyle}>Welcome
+                back,</Text>
+              <Text style={styles.overlayTextStyle2}>Log In to
+                continue</Text>
             </View>
-        );
-    }
+          </View>
+          <View style={styles.container}>
+            <Card style={styles.cardStyle}>
+              <Card.Title title="Login"
+                          titleStyle={styles.cardTitleStyle}/>
+
+              <Card.Content>
+                <TextInput
+                    label='Email'
+                    clear
+                    value={this.state.email}
+                    onChangeText={this.handlechange('phone')}
+                    mode="outlined"
+                    icon="camera"
+                    style={{padding: 10}}
+                    theme={{
+                      colors: {
+                        primary: '#207cd0',
+                        background: '#fff'
+                      }
+                    }}
+
+                />
+                <TextInput
+                    label='Password'
+                    focused
+                    clear
+                    value={this.state.name}
+                    onChangeText={this.handlechange('name')}
+                    mode="outlined"
+                    style={{padding: 10}}
+                    theme={{
+                      colors: {
+                        primary: '#207cd0',
+                        background: '#fff'
+                      }
+                    }}
+
+                />
+                <TouchableOpacity>
+                  <View style={styles.signUpTextView}>
+                    <Text onPress={() => this.props.navigation.navigate("signUp")}
+                          style={styles.signUpText}>
+                      New User? Sign Up.
+                    </Text>
+                  </View>
+
+                </TouchableOpacity>
+              </Card.Content>
+
+              <Card.Actions style={{justifyContent: 'center'}}>
+                <TouchableOpacity onPress={this.submit}>
+                  <IconButton
+                      icon="arrow-right"
+                      size={30}
+                      color="white"
+                      style={styles.IconButton}/>
+                </TouchableOpacity>
+
+
+              </Card.Actions>
+            </Card>
+
+
+          </View>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 30,
-        justifyContent: 'center',
-        marginTop: -100
-    },
-    input: {
-        padding: 10,
-        borderWidth: 1,
-        width: '90%',
-        marginBottom: 10
+  BtnType: {
+    color: 'darkblue',
+    fontSize: 20
+  },
 
-    },
-    BtnType: {
-        color: 'darkblue',
-        fontSize: 20
+  cardStyle: {
+    elevation: 8,
+    marginTop: -350,
+    borderRadius: 12,
+    padding: 8
+  },
 
-    },
-    IconButton: {
-        backgroundColor: "#8334eb",
-        borderRadius: 30,
-        height: 60,
-        width: 60,
-        marginBottom: -35,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  cardTitleStyle: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    fontSize: 24,
+    color: "#207cd0"
+  },
+
+
+  container: {
+    flex: 7,
+    backgroundColor: '#fff',
+    padding: 30,
+    justifyContent: 'center',
+    marginTop: 0
+  },
+
+  container2: {
+    flex: 4,
+  },
+
+  IconButton: {
+    backgroundColor: "#fa1800",
+    borderRadius: 35,
+    elevation: 4,
+    height: 70,
+    width: 70,
+    marginBottom: -45,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    width: '90%',
+    marginBottom: 10
+  },
+
+
+  overlayStyle: {
+    padding: 30,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: 'rgba(36,92,208,0.6)'
+  },
+
+  overlayTextStyle: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "yellow",
+    fontStyle: "italic"
+  },
+
+  overlayTextStyle2: {
+    marginBottom: 40,
+    fontSize: 18,
+    fontWeight: "100",
+    color: "white"
+  },
+
+  signUpText: {
+    justifyContent: 'flex-end',
+    color: '#207cd0',
+    paddingLeft: 20,
+    paddingBottom: 4
+  },
+
+  signUpTextView: {
+    justifyContent: 'flex-end',
+    color: 'red'
+  },
+
+
 });
