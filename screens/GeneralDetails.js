412bab8e90b1519095a11a12324dc912b4ff8525
killbotXD <sonawane.1@iitj.ac.in>
2020-03-27 12:51:40 +0530

    remove reused style and import styles from general styles

--- screens/GeneralDetails.js
+++ screens/GeneralDetails.js
@@ -10,7 +10,7 @@ import {
   Card,
   TextInput,
 } from 'react-native-paper';
-
+let styles = require('../styles/GeneralStyles');
 
 export default class GeneralDetails extends Component {
 
@@ -21,7 +21,7 @@ export default class GeneralDetails extends Component {
     Age: '', Gender: 'Female'
   };
 
-  handlechange = key => val => {
+  handleChange = key => val => {
     this.setState({
       [key]: val
     })
@@ -75,7 +75,7 @@ export default class GeneralDetails extends Component {
                 <TextInput
 
                     value={this.state.name}
-                    onChangeText={this.handlechange('name')}
+                    onChangeText={this.handleChange('name')}
                     mode="outlined"
                     style={{padding: 10}}
                     theme={{
@@ -99,13 +99,13 @@ export default class GeneralDetails extends Component {
                     clear
                     label="Phone Number"
                     value={this.state.phone}
-                    onChangeText={this.handlechange('phone')}
+                    onChangeText={this.handleChange('phone')}
                     keyboardType='number-pad'
                 />
                 <TextInput
 
                     value={this.state.id}
-                    onChangeText={this.handlechange('id')}
+                    onChangeText={this.handleChange('id')}
                     mode="outlined"
                     style={{padding: 10}}
                     theme={{
@@ -119,7 +119,7 @@ export default class GeneralDetails extends Component {
                 />
                 <TextInput
                     value={this.state.Age}
-                    onChangeText={this.handlechange('Age')}
+                    onChangeText={this.handleChange('Age')}
                     mode="outlined"
                     style={{padding: 10}}
                     theme={{
@@ -177,97 +177,3 @@ export default class GeneralDetails extends Component {
     );
   }
 }
-const styles = StyleSheet.create({
-  BtnType: {
-    backgroundColor: 'blue', padding: 8, borderRadius: 10
-  }, BtnTypeGray: {
-    padding: 8,
-    backgroundColor: 'gray', borderRadius: 10
-  },
-
-  cardStyle: {
-    elevation: 16,
-    marginTop: -80,
-    borderRadius: 12,
-    padding: 8,
-    paddingTop: 16
-  },
-
-  cardTitleStyle: {
-    paddingLeft: 10,
-    paddingTop: 10,
-    fontSize: 24,
-    color: "#207cd0"
-  },
-
-
-  container: {
-    flex: 7,
-    backgroundColor: '#fff',
-    padding: 16,
-    marginTop: -30
-  },
-
-  container2: {
-    flex: 4,
-  },
-
-  IconButton: {
-    backgroundColor: "#fa1800",
-    borderRadius: 35,
-    elevation: 4,
-    height: 70,
-    width: 70,
-    marginBottom: -45,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  input: {
-    padding: 10,
-    borderWidth: 1,
-    width: '90%',
-    marginBottom: 10
-  },
-
-
-  overlayStyle: {
-    padding: 30,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: 'rgb(6,55,159)'
-  },
-
-  overlayTextStyle: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "yellow",
-    fontStyle: "italic"
-  },
-
-  overlayTextStyle2: {
-    marginBottom: 40,
-    fontSize: 18,
-    fontWeight: "100",
-    color: "white"
-  },
-
-  signUpText: {
-    justifyContent: 'flex-end',
-    color: '#207cd0',
-    paddingLeft: 20,
-    paddingBottom: 4
-  },
-
-  signUpTextView: {
-    justifyContent: 'flex-end',
-    color: 'red'
-  },
-
-
-});
--- screens/SpecialDetails.js
+++ screens/SpecialDetails.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react';
 import {
-  StyleSheet,
   Alert,
   Text,
   TouchableOpacity, View, Picker
 } from 'react-native';
-
+let styles = require('../styles/GeneralStyles');
 import {
   Button,
   Card,
   TextInput
 } from 'react-native-paper';
 
-let receivedParams = ''
+let receivedParams = '';
 
 export default class SpecialDetails extends Component {
 
@@ -169,97 +168,3 @@ export default class SpecialDetails extends Component {
     );
   }
 }
-const styles = StyleSheet.create({
-  BtnType: {
-    backgroundColor: 'blue', padding: 8, borderRadius: 10
-  }, BtnTypeGray: {
-    padding: 8,
-    backgroundColor: 'gray', borderRadius: 10
-  },
-
-  cardStyle: {
-    elevation: 16,
-    marginTop: -80,
-    borderRadius: 12,
-    padding: 8,
-    paddingTop: 16
-  },
-
-  cardTitleStyle: {
-    paddingLeft: 10,
-    paddingTop: 10,
-    fontSize: 24,
-    color: "#207cd0"
-  },
-
-
-  container: {
-    flex: 7,
-    backgroundColor: '#fff',
-    padding: 16,
-    marginTop: -30
-  },
-
-  container2: {
-    flex: 4,
-  },
-
-  IconButton: {
-    backgroundColor: "#fa1800",
-    borderRadius: 35,
-    elevation: 4,
-    height: 70,
-    width: 70,
-    marginBottom: -45,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  input: {
-    padding: 10,
-    borderWidth: 1,
-    width: '90%',
-    marginBottom: 10
-  },
-
-
-  overlayStyle: {
-    padding: 30,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    backgroundColor: 'rgb(6,55,159)'
-  },
-
-  overlayTextStyle: {
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "yellow",
-    fontStyle: "italic"
-  },
-
-  overlayTextStyle2: {
-    marginBottom: 40,
-    fontSize: 18,
-    fontWeight: "100",
-    color: "white"
-  },
-
-  signUpText: {
-    justifyContent: 'flex-end',
-    color: '#207cd0',
-    paddingLeft: 20,
-    paddingBottom: 4
-  },
-
-  signUpTextView: {
-    justifyContent: 'flex-end',
-    color: 'red'
-  },
-
-
-});
